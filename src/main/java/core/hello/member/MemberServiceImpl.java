ab33021dfd51cc6dfdcdf1fa8285169924fc00ce
BreezeBM <seyya77@gmail.com>
2023-06-14 16:01:24 +0900

    feat: 회원 도메인 실행과 테스트

--- src/main/java/core/hello/member/MemberServiceImpl.java
+++ src/main/java/core/hello/member/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package core.hello.member;
 
 public class MemberServiceImpl implements MemberService {
+
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
