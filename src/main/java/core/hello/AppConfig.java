b8b27754e6b0ceae57a929fd74eb95c0bf3e6ba4
BreezeBM <seyya77@gmail.com>
2023-06-15 19:36:05 +0900

    feat: AppConfig 리팩터링(역할과 구현이 보이도록)

--- src/main/java/core/hello/AppConfig.java
+++ src/main/java/core/hello/AppConfig.java
@@ -1,6 +1,8 @@
 package core.hello;
 
+import core.hello.discount.DiscountPolicy;
 import core.hello.discount.FixDiscountPolicy;
+import core.hello.member.MemberRepository;
 import core.hello.member.MemberService;
 import core.hello.member.MemberServiceImpl;
 import core.hello.member.MemoryMemberRepository;
@@ -8,11 +10,19 @@ import core.hello.order.OrderService;
 import core.hello.order.OrderServiceImpl;
 
 public class AppConfig {
+    public  MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
