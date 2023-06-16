e61b3fc6dfa463ec90ab761996af3efbfcc86074
BreezeBM <seyya77@gmail.com>
2023-06-16 14:36:37 +0900

    feat: 스프링으로 전환하기

--- src/main/java/core/hello/AppConfig.java
+++ src/main/java/core/hello/AppConfig.java
@@ -8,20 +8,24 @@ import core.hello.member.MemberServiceImpl;
 import core.hello.member.MemoryMemberRepository;
 import core.hello.order.OrderService;
 import core.hello.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public  MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
-
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
