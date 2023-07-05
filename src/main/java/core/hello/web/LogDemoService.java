1ca1e67976eaf365c338ffc4c73c329015270357
BreezeBM <seyya77@gmail.com>
2023-07-06 08:38:37 +0900

    빈 스코프 - request 스코프 예제 만들기

---
+++ src/main/java/core/hello/web/LogDemoService.java
@@ -0,0 +1,15 @@
+package core.hello.web;
+
+import core.hello.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id =" + id);
+    }
+}
