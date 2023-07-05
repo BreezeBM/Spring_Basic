2c61b3e0f84ff70eeb7173759f5baf8b7fafe9d8
BreezeBM <seyya77@gmail.com>
2023-07-05 16:40:57 +0900

    빈 스코프 - 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/core/hello/scope/PrototypeProviderTest.java
+++ src/test/java/core/hello/scope/PrototypeProviderTest.java
@@ -4,16 +4,16 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
-import javax.inject.Provider;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonWithPrototypeTest1 {
+public class PrototypeProviderTest {
+
     @Test
-    void singletonClientUsePrototype() {
+    void providerTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
@@ -27,29 +27,16 @@ public class SingletonWithPrototypeTest1 {
 
     static class ClientBean {
         @Autowired
-        private Provider<PrototypeBean> prototypeBeanProvider; // ObjectProvider -> ObjectFactory도 가능
+        private ApplicationContext ac;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
     }
 
-
-
-    @Test
-    void prototypeFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        PrototypeBean prototypeBean1 = new PrototypeBean();
-        prototypeBean1.addCount();
-        assertThat(prototypeBean1.getCount()).isEqualTo(1);
-
-        PrototypeBean prototypeBean2 = new PrototypeBean();
-        prototypeBean2.addCount();
-        assertThat(prototypeBean2.getCount()).isEqualTo(1);
-    }
 
     @Scope("prototype")
     static class PrototypeBean {
