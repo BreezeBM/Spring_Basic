6492a94bfe960f52f6b7ba983fdf1b288604711a
BreezeBM <seyya77@gmail.com>
2023-07-06 09:03:44 +0900

    빈 스코프 - 스코프와 Provider

--- src/main/java/core/hello/web/LogDemoService.java
+++ src/main/java/core/hello/web/LogDemoService.java
@@ -2,14 +2,16 @@ package core.hello.web;
 
 import core.hello.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
-        myLogger.log("service id =" + id);
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + id);
     }
 }
